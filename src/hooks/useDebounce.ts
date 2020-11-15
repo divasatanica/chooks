c7f065e993d377f10417cddf55c832b10905153a
divasatanica <jayhebes@vip.qq.com>
2020-11-15 18:14:13 +0800

    fix: store debounced reference with useRef

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,10 +1,13 @@
-import { useCallback } from 'react';
+import { useRef } from 'react';
 import { debounce } from '@/utils/tools';
 
-function useDebounce(fn: (...args: unknown[]) => unknown, delay: number): (...args: unknown[]) => unknown {
-  const debounced = useCallback(debounce(fn, delay), [fn, delay]);
+function useDebounce(fn: (...args: unknown[]) => any, delay: number, thisValue?: unknown): (...args: unknown[]) => unknown | unknown {
+  const fnRef = useRef(fn);
+  fnRef.current = fn;
+  const debounced = debounce(fnRef.current, delay, thisValue);
+  const debounceRef = useRef(debounced);
 
-  return debounced;
+  return debounceRef.current;
 }
 
 export {
