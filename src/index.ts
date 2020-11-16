6a37088a42b7bd8ce6d53a33dcf25a029e6e268b
divasatanica <jayhebes@vip.qq.com>
2020-11-16 15:21:40 +0800

    feat: add useToggle hook

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { useUrlParams } from './hooks/useUrlParams';
 import { useLocalStorage } from './hooks/useLocalStorage';
 import { useDebounce } from './hooks/useDebounce';
 import { useCountdown } from './hooks/useCountdown';
+import { useToggle } from './hooks/useToggle';
 
 export {
   useChangedProps,
   useUrlParams,
   useLocalStorage,
   useDebounce,
-  useCountdown
+  useCountdown,
+  useToggle
 };
